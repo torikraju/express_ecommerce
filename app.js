943f85f9047ce6ce70cd1b8f07b4173bd06fbf29
torikraju <torikraju@gmail.com>
2019-07-02 19:38:28 +0600

    Adding a 404 Error Page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ const port = 3001;
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>');
+});
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 
