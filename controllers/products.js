b8b0c9a270a252df4584393ea1b3dde4edc9146f
torikraju <torikraju@gmail.com>
2019-07-03 21:10:25 +0600

    Storing and Fetching Data in Files Via the Model

--- controllers/products.js
+++ controllers/products.js
@@ -18,12 +18,14 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('shop', {
-        prods: Product.fetchAll(),
-        pageTitle: 'Shop',
-        path: '/',
-        hasProducts: Product.fetchAll().length > 0,
-        activeShop: true,
-        productCSS: true
+    Product.fetchAll(products => {
+        res.render('shop', {
+            prods: products,
+            pageTitle: 'Shop',
+            path: '/',
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCSS: true
+        });
     });
 };
--- models/product.js
+++ models/product.js
@@ -1,4 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path');
+
 const products = [];
+const _path = path.join(rootDir, 'data', 'products.json');
 
 
 module.exports = class Product {
@@ -8,12 +13,23 @@ module.exports = class Product {
     }
 
     save() {
-        products.push(this)
+        fs.readFile(_path, (err, fileContent) => {
+            let products = [];
+            if (!err) {
+                products = JSON.parse(fileContent);
+            }
+            products.push(this);
+            fs.writeFile(_path, JSON.stringify(products), (err) => console.log(err))
+        });
     }
 
-
-    static fetchAll() {
-        return products;
+    static fetchAll(cb) {
+        fs.readFile(_path, (err, fileContent) => {
+            if (err) {
+                cb([]);
+            }
+            cb(JSON.parse(fileContent));
+        });
     }
 
 
