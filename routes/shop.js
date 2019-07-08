030d5c96f0ef937066d4eceba6ed518bbc8da817
torikraju <torikraju@gmail.com>
2019-07-08 21:34:30 +0600

    Getting Orders

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -20,6 +18,6 @@ router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
 router.post('/create-order', shopController.postOrder);
 
-// router.get('/orders', shopController.getOrders);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
