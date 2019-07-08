d8a12d4d18aa4d96f5baae77f0afb95534e52510
torikul alam <torikul@softitsecurity.com>
2019-07-08 17:10:57 +0600

    Creating New Users

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const shopRoutes = require('./routes/shop');
 
 const errorController = require('./controllers/error');
 
+const User = require('./models/user');
+
 
 const app = express();
 const port = 3001;
@@ -21,6 +23,15 @@ app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 // for serving static file
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('5d23245eb9bdf42f457370df')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
--- models/user.js
+++ models/user.js
@@ -1,16 +1,26 @@
-const Sequelize = require('sequelize');
+const { ObjectId } = require('mongodb');
 
-const sequelize = require('../util/database');
+const { getDb } = require('../util/database');
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('users')
+      .insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection('users')
+      .findOne({ _id: new ObjectId(userId) });
+  }
+}
 
 module.exports = User;
