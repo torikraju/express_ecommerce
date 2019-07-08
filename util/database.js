3d40f12fb9a38db2c7bf59dfbabfce596688125b
torikraju <torikraju@gmail.com>
2019-07-08 20:53:07 +0600

    Storing Multiple Products in the Cart

--- util/database.js
+++ util/database.js
@@ -4,8 +4,10 @@ const { MongoClient } = mongodb;
 
 let _db;
 
+const _dbUrl = 'mongodb+srv://torikraju:fKZPEn8Nuak4JFd@cluster0-shyre.mongodb.net/express_ecommerce?retryWrites=true&w=majority';
+
 const mongoConnect = (callBack) => {
-  MongoClient.connect('mongodb+srv://torikraju:fKZPEn8Nuak4JFd@cluster0-shyre.mongodb.net/express_ecommerce?retryWrites=true&w=majority')
+  MongoClient.connect(_dbUrl, { useNewUrlParser: true })
     .then(client => {
       _db = client.db();
       callBack(client);
