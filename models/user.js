6e6b440259943279e3cfdb7290173c753794d7c2
torikul alam <torikul@softitsecurity.com>
2019-07-09 12:29:23 +0600

    working-on-the-shopping-cart

--- models/user.js
+++ models/user.js
@@ -28,4 +28,25 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.methods.addToCart = function (product) {
+  const cartProductIndex = this.cart.items.findIndex(cp => cp.productId.toString() === product._id.toString());
+  let newQuantity = 1;
+  const updatedCartItems = [...this.cart.items];
+
+  if (cartProductIndex >= 0) {
+    newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+    updatedCartItems[cartProductIndex].quantity = newQuantity;
+  }
+  else {
+    updatedCartItems.push({
+      productId: product._id,
+      quantity: newQuantity
+    });
+  }
+  this.cart = {
+    items: updatedCartItems
+  };
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
