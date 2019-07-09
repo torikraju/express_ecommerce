2bc903f85cfec2f04059a4363d6b1aca31badb32
torikraju <torikraju@gmail.com>
2019-07-09 19:09:57 +0600

    Creating the Login Form

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const { DB_URL } = require('./util/string');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
+
 
 const errorController = require('./controllers/error');
 
@@ -36,6 +38,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 // catch 404 error
 app.use(errorController.get404);
