d7e6339c09306e8343bcca0334bd2f934a1c69cd
torikul alam <torikul@softitsecurity.com>
2019-07-10 15:09:12 +0600

    Creating the Token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 
 const User = require('../models/user');
 const { SENDGRID_API_KEYS } = require('../util/string');
+const { SIGN_UP_SUCCESS, RESET_PASSWORD, NO_REPLAY } = require('../util/emailRelatedStuff');
 
 const transporter = nodemailer.createTransport(
   sendgridTransport({
@@ -97,13 +99,51 @@ exports.postSignup = (req, res) => {
           user.save()
             .then(() => transporter.sendMail({
               to: email,
-              from: 'noReplay@express_ecommerce.com',
+              from: NO_REPLAY,
               subject: 'Signup succeeded!',
-              html: '<h1>You successfully signed up!</h1>'
+              html: SIGN_UP_SUCCESS
             }))
             .then(() => res.redirect('/'))
             .catch(e => console.log(e));
         });
     })
     .catch(err => console.log(err));
+};
+
+exports.getReset = (req, res) => {
+  let message = req.flash('error');
+  message = (message.length > 0) ? message[0] : null;
+  res.render('auth/reset', {
+    path: '/reset',
+    pageTitle: 'Reset Password',
+    errorMessage: message
+  });
+};
+
+exports.postReset = (req, res) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.log(err);
+      return res.redirect('/reset');
+    }
+    const token = buffer.toString('hex');
+    User.findOne({ email: req.body.email })
+      .then(user => {
+        if (!user) {
+          req.flash('error', 'No account with that email found.');
+          return res.redirect('/reset');
+        }
+        user.resetToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then(() => transporter.sendMail({
+        to: req.body.email,
+        from: NO_REPLAY,
+        subject: 'Password reset',
+        html: `<p>You requested a password reset</p><p>Click this <a href="http://localhost:3001/reset/${token}">link</a> to set a new password.</p>`
+      }))
+      .then(() => res.redirect('/'))
+      .catch(e => console.log(e));
+  });
 };
---
+++ util/emailRelatedStuff.js
@@ -0,0 +1,3 @@
+exports.SIGN_UP_SUCCESS = '<h1>You successfully signed up!</h1>';
+
+exports.NO_REPLAY = 'noReplay@express_ecommerce.com';
