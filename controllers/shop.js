5530c4730292001112f14d2c51ea9efeb50aac45
torikraju <torikraju@gmail.com>
2019-07-12 16:09:19 +0600

    Streaming Data

--- controllers/shop.js
+++ controllers/shop.js
@@ -136,15 +136,13 @@ exports.getInvoice = (req, res, next) => {
       if (order.user.userId.toString() !== req.user._id.toString()) return next(new Error('Unauthorized'));
       const invoiceName = `invoice-${orderId}.pdf`;
       const invoicePath = path.join('data', 'invoices', invoiceName);
-      fs.readFile(invoicePath, (err, data) => {
-        if (err) return next(err);
-        res.setHeader('Content-Type', 'application/pdf');
-        res.setHeader(
-          'Content-Disposition',
-          `inline; filename="${invoiceName}"`
-        );
-        res.send(data);
-      })
-        .catch(err => next(err));
-    });
+      const file = fs.createReadStream(invoicePath);
+      res.setHeader('Content-Type', 'application/pdf');
+      res.setHeader(
+        'Content-Disposition',
+        `inline; filename="${invoiceName}"`
+      );
+      file.pipe(res);
+    })
+    .catch(err => next(err));
 };
