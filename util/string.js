4e6b608add19e62a510c842173e45f8480c8c9c9
torikraju <torikraju@gmail.com>
2019-07-15 19:26:08 +0600

    adding db url with key

---
+++ util/string.js
@@ -0,0 +1,5 @@
+exports.MONGODB_URI = 'mongodb+srv://torikraju:fKZPEn8Nuak4JFd@cluster0-shyre.mongodb.net/express_ecommerce?retryWrites=true&w=majority';
+
+exports.SENDGRID_API_KEYS = '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';
+
+exports.STRIPE_SECRET_KEY = 'sk_test_M2Ho1BLmn1QtcT0AusMvGoN800yHLCmZ7O';
